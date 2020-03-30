342ea6e2cde20c9674666da87c88f9cff816b6a3
Jethet <henriette.hettinga@gmail.com>
2020-03-30 18:11:40 +0200

    Create Weather, Form and Titles components, create .env and hide API_KEY

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from "react";
 
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
 require("dotenv").config();
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+const url = `http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`;
+
 class App extends React.Component {
+  getWeather = async (e) => {
+    e.preventDefault()
+    const api_call = await fetch(url);
+    const data = await api_call.json();
+    console.log(data);
+    
+  };
+
   render() {
     return (
-      <div>This is the app</div>
-    )
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather} />
+        <Weather />
+      </div>
+    );
   }
 }
 
-export default App
+export default App;
