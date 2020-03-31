3813c2f22c2f670399f0815dbb03b9f9d7c53252
Jethet <henriette.hettinga@gmail.com>
2020-03-31 11:58:03 +0200

    Add error message, add condition for form values to show, change Form, Titles and Weather class components into function components

--- src/App.js
+++ src/App.js
@@ -7,15 +7,45 @@ import Weather from "./components/Weather";
 require("dotenv").config();
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-const url = `http://api.openweathermap.org/data/2.5/weather?q=Manchester,uk&appid=${API_KEY}&units=metric`;
 
 class App extends React.Component {
-  getWeather = async (e) => {
-    e.preventDefault()
-    const api_call = await fetch(url);
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
+
     const data = await api_call.json();
-    console.log(data);
-    
+
+    if (city && country) {
+      this.setState({
+        city: data.name,
+        country: data.sys.country,
+        temperature: data.main.temp,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        city: undefined,
+        country: undefined,
+        temperature: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter city and country."
+      });
+    }
   };
 
   render() {
@@ -23,7 +53,14 @@ class App extends React.Component {
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          city={this.state.city}
+          country={this.state.country}
+          temperature={this.state.temperature}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-class Weather extends React.Component {
-    render() {
-      return <div>Weather Component</div>;
-    }
+const Weather = (props) => {
+      return (
+          // To avoid fields below from loading on landing page, set condition that fields need to
+          // be filled in before they can be shown (with &&)
+      <div>
+      {props.city && props.country && <p>Location: {props.city} {props.country}</p>}
+      {props.temperature && <p>Temperature: {props.temperature}</p>}
+      {props.humidity && <p>Humidity: {props.humidity}</p>}
+      {props.description && <p>Conditions: {props.description}</p>}
+      {props.error && <p>{props.error}</p>}
+      </div>
+      )
   }
   
   export default Weather;
